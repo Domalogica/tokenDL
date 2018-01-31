b00f9e07d404e00c23dd60dbfd36872ddbe5598e
frontjss <frontjss@gmail.com>
2018-01-31 10:50:21 +0300

    new

--- app.py
+++ app.py
@@ -61,7 +61,7 @@ Email: %s
         requests.post(url)   
         url = "https://api.telegram.org/bot527562365:AAFDyCml1bgH7D5mvng6KcxKI-dTvAN6Ybc/sendMessage?chat_id=65472004&text=%s" % (text)
         requests.post(url)   
-        url = "https://api.telegram.org/bot527562365:AAFDyCml1bgH7D5mvng6KcxKI-dTvAN6Ybc/sendMessage?chat_id=-263564659&text=%s" % (text)
+        url = "https://api.telegram.org/bot527562365:AAFDyCml1bgH7D5mvng6KcxKI-dTvAN6Ybc/sendMessage?chat_id=-303230127&text=%s" % (text)
         requests.post(url)   
         url = "https://api.telegram.org/bot527562365:AAFDyCml1bgH7D5mvng6KcxKI-dTvAN6Ybc/sendMessage?chat_id=34436430&text=%s" % (text)
         requests.post(url)   
