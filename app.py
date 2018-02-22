10e25a4c7a81687c3a1b9d58c92aa031c32d4997
frontjss <frontjss@gmail.com>
2018-02-22 15:56:30 +0300

    new

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ mysql.init_app(app)
 def index():
     response = requests.get('http://dl.kaspsoft.com/?r=api/get-balans&authCod=lkfghfghljIFyDOP')
     response = json.loads(response.content.decode("utf-8"))
-    return response = sorted(response.items(), key=lambda x: x[0])
+    response = sorted(response.items(), key=lambda x: x[0])
+    return response
 
 print(index())
 
